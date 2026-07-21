3b6f181d879ed74653ee342be8cd4b7ef372e604
Harsh Katiyar <hkatiyar370@gmail.com>
2026-07-21 12:01:56 +0530

    Time: 1 ms (29.99%) | Memory: 47.5 MB (20.24%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int i=0;
+        
+          while(i<arr.length  ){
+              int correct = arr[i] ;
+            if(arr[i]<arr.length && arr[i] != arr[correct]){
+                 Swap(arr,i,correct) ;
+            }else{
+              i++;
+            }
+          }
+          for(int index=0;index<arr.length;index++){
+                if(arr[index]!=index){
+                  return index;
+                }
+          }
+          return arr.length;
+    }
+
+    public  void Swap(int arr[],int start, int end){
+                       int temp = arr[start] ;
+                        arr[start]=arr[end] ;
+                        arr[end]=temp ;
+  }
+}
