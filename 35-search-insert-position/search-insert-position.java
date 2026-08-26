06dcabf20f1eb99791b6c4c81d2851f3eed678ed
Harsh Katiyar <hkatiyar370@gmail.com>
2026-08-26 19:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (48.57%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -3,19 +3,19 @@ class Solution {
           int s = 0 ;
           int n = nums.length;
           int e = n-1;
-          int anss =n;
+          int ans =n;
 
           while(s<=e){
             int mid = s+(e-s)/2;
             if(nums[mid]==target){
                 return mid ;
             }else if(nums[mid]>target){
-                  anss = mid;
+                  ans = mid;
                    e=mid-1;
             }else{
                 s = mid+1;
             }
           }
-          return anss;
+          return ans;
     }
 }
